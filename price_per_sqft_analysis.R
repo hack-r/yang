c9e9bab0c0d9b8de766a1d304e1ba2a64c46ac2a
Jason Miller <jmillerlaureate@gmail.com>
2016-04-20 14:41:26 -0400

    .

--- price_per_sqft_analysis.R
+++ price_per_sqft_analysis.R
@@ -4,6 +4,9 @@ setwd("C://users//jmiller//Desktop//yang")
 
 # Functions and Libraries -------------------------------------------------
 pacman::p_load(data.table, RDSTK, RgoogleMaps, rvest, stringi, sqldf, XML, zipcode, zoo)
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
 
 # Data --------------------------------------------------------------------
 zs  <- readRDS("zs.RDS")
@@ -91,22 +94,37 @@ mls$City[mls$City == "DUAR"] <- "DUARTE"
 mls$City[mls$City == "CULV"] <- "CULVER CITY"
 mls$City[mls$City == "ARCD"] <- "unknown"
 mls$City[mls$City == "AZU"]  <- "AZUSA"
-  
-  
+
 mls$City <- tolower(mls$City)
 
-write.csv(mls, "mls_cities.csv", row.names = F)
-# mls.zip$zip[mls.zip$zip == "unknown"] <- "91304" # manual lookup
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "huntington paramountk"] <- "90255"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "commercepton"]          <- "90221"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "cudahy"]                <- "90201"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "los angelesm"]          <- "90638"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "santa fe san pedrorings"] <- "90670"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "signal hill"] <- "90755"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "highland paramountk"] <- "90043"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city == "pomona "] <- "91766"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city_code == "COMMERCE"]<- "90040"
-# mls.zip$zip[is.na(mls.zip$zip) & mls.zip$city_code == "BERMUDA DUNES"]<- "91706"
+write.csv(mls, "mls_cities.csv", row.names = F) # several city code replacements made in Excel
+
+mls <- read.csv("mls_geocoded.csv")
+mls <- mls[mls$S == "S",]
+
+mls$census_address <- as.character(mls$census_address)
+mls$zip            <- substrRight(mls$census_address, 5)
+mls$zip            <- as.numeric(mls$zip)
+
+mls$zip[mls$St.Name == "OCEAN PARK BL"] <- 90405
+mls$zip[mls$St.Name == "BEVERLY"]       <- 90022
+mls$zip[mls$St.Name == "Beverly"]       <- 90022
+mls$zip[mls$City == "db"]               <- 91765 
+mls$zip[mls$St.Name == "long beach"]    <- 90802
+mls$zip[mls$St.Name == "Business Center DR"] <- 91706  
+mls$zip[mls$St.Name == "Alhambra AV"]   <- 90032 
+mls$zip[mls$St.Name == "1st AV"]        <-  91006
+mls$zip[mls$City == "bermuda dunes"]    <-  91706
+mls$zip[mls$City == "rsmd"]             <-  91770
+mls$zip[mls$City == "clar"]             <-  91711
+mls$zip[mls$St.Name == "Adams"]         <-  90016
+mls$zip[mls$St.Name == "San Pedro ST #269"]   <-  90012
+mls$zip[mls$Area == "C34"]      <-  90062
+mls$zip[mls$City == "alta"]     <-  91001
+mls$zip[mls$City == "corvina"]  <-  91724
+mls$zip[mls$Area == "605"]   <- 91006 
+mls$zip[mls$Area == "633"]   <- 91744 
+mls$zip[mls$Area == "641"]   <- 91754
 
 # Final residential data set
 zsdf <- as.data.frame(zs)
@@ -132,7 +150,20 @@ res$sold_date[is.na(res$sold_date)]       <- mean(res$sold_date, na.rm = T)
 res$price_per_sqft[is.na(res$price_per_sqft)] <- res$sold_price[is.na(res$price_per_sqft)]/res$sqft[is.na(res$price_per_sqft)]
 
 # Final commericial data set
-com <- mls.zip[c("sold_price", "sold_date","Sqft", "zip", "price_per_sqft")]
+mls$price <- as.character(mls$L.S.Price)
+mls$price <- gsub("\\$", "", mls$price)
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+mls$price <- trim(mls$price)
+mls$price <- gsub(",", "", mls$price)
+mls$price <- as.numeric(mls$price)
+com <- sqldf("select price as sold_price, Date as sold_date, Sqft as sqft, zip from mls")
+
+com$sqft <- as.character(com$sqft)
+com$sqft <- gsub(",", "", com$sqft)
+com$sqft <- trim(com$sqft)
+com$sqft <- as.numeric(com$sqft)
+com$sqft[is.na(com$sqft)] <- mean(com$sqft)
+com$price_per_sqft <- com$sold_price/com$sqft
 
 colnames(com) <- colnames(res)
 
@@ -166,17 +197,42 @@ res$commercial <- 0
 com$commercial <- 1
 res_com <- rbind(res,com)
 
+res_com$zip_flag_90813 <- 0
+res_com$zip_flag_90813[res_com$zip == "90813"] <- 1
+res_com$zip_flag_90802 <- 0
+res_com$zip_flag_90802[res_com$zip == "90802"] <- 1
+res_com$zip_flag_90804 <- 0
+res_com$zip_flag_90804[res_com$zip == "90804"] <- 1
+res_com$zip_flag_90806 <- 0
+res_com$zip_flag_90806[res_com$zip == "90806"] <- 1
+res_com$zip_flag_90805 <- 0
+res_com$zip_flag_90805[res_com$zip == "90805"] <- 1
+res_com$zip_flag_90062 <- 0
+res_com$zip_flag_90062[res_com$zip == "90062"] <- 1
+res_com$zip_flag_91326 <- 0
+res_com$zip_flag_91326[res_com$zip == "91326"] <- 1
+res_com$zip_flag_91344 <- 0
+res_com$zip_flag_91344[res_com$zip == "91344"] <- 1
+res_com$zip_flag_91602 <- 0
+res_com$zip_flag_91602[res_com$zip == "91602"] <- 1
+res_com$zip_flag_90067 <- 0
+res_com$zip_flag_90067[res_com$zip == "90067"] <- 1
+res_com$zip_flag_91601 <- 0
+res_com$zip_flag_91601[res_com$zip == "91601"] <- 1
+res_com$zip_flag_91604 <- 0
+res_com$zip_flag_91604[res_com$zip == "91604"] <- 1
+
 saveRDS(res_com, "res_com.RDS")
 write.csv(res_com, "res_com.csv", row.names = F)
 
 # Analysis ----------------------------------------------------------------
-plot(density(com$price_per_sqft))
-lines(density(res$price_per_sqft), col="red")
-
-ppsf.mod  <- glm(price_per_sqft ~ ., data = res_com)
-price.mod <- glm(sold_price ~ ., data = res_com)
+ppsf.mod  <- lm(price_per_sqft ~ sold_price+sold_date+sqft+commercial+zip_flag_90813+zip_flag_90802+zip_flag_90806+zip_flag_90804+zip_flag_90805+zip_flag_90062+zip_flag_91326+zip_flag_91344+zip_flag_91602+zip_flag_90067+zip_flag_91601+zip_flag_91604
+                   , data = res_com)
+price.mod <- lm(sold_price ~ price_per_sqft + sold_date+sqft+commercial+zip_flag_90813+zip_flag_90802+zip_flag_90806+zip_flag_90804+zip_flag_90805+zip_flag_90062+zip_flag_91326+zip_flag_91344+zip_flag_91602+zip_flag_90067+zip_flag_91601+zip_flag_91604, data = res_com)
 summary(ppsf.mod)
-summary(price.mod) # shows comm vs residential is highly insignificant controlling for other factors
+summary(price.mod)
 
 # Data Visualization ------------------------------------------------------
+plot(density(com$price_per_sqft))
+lines(density(res$price_per_sqft), col="red")
 
